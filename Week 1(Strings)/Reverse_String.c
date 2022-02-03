412c7ce20afb47c5ae8a6f772cb2d7856da48f04
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2022-02-03 22:39:07 +0530

    Create Reverse_String.c

---
+++ Week 1(Strings)/Reverse_String.c
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+   char temp;
+   string str;
+   getline(cin,str);
+   
+  int a=str.size();
+  
+  for(int i=0; i<a/2; i++)
+  {
+      temp=str[i];
+      str[i]=str[a-i-1];
+      str[a-i-1]=temp;
+  }
+  
+  cout<<str;
+   
+   return 0;
+}
