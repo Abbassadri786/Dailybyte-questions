bc8e5233f55fb4919b645ce03ff129e0099b5138
Abbas Sadriwala <90386455+Abbassadri786@users.noreply.github.com>
2022-02-09 15:00:43 +0530

    Update Correct_Capitalization.cpp

--- Correct_Capitalization.cpp
+++ Correct_Capitalization.cpp
@@ -8,16 +8,23 @@ int main()
   string str;
   cin>>str;
   
-  int a=str.size(),found=0;
+  int a=str.size(),found=0,count=0;
+  
   for(int i=0; i<a; i++)
   {
       if(str[i]>='A' && str[i]<='Z')
       {
+          count++;
+          
+      }
+      else if(str[0]>='A' && str[0]<='Z')
+      {
           found=1;
           break;
       }
+      
   }
-  if(found==1)
+  if(count==0 || found==1)
   {
       cout<<"true"<<endl;
   }
